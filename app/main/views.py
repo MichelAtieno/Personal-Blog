23bc6dc4210d5d3aa3cf5cbe08f8891fe0e1f5a3
Michel Atieno <mishqamish@gmail.com>
2018-09-13 21:52:01 +0300

    Subscription form successfully sends email to subscriber

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,25 @@
-from flask import render_template
+from flask import render_template, redirect, url_for,flash
 from . import main
+from .forms import SubscriberForm
+from ..models import User, Subscribers
+from ..email import mail_message
+from .. import db
 
-@main.route('/')
+@main.route('/',methods=['GET', 'POST'])
 def index():
 
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    title= "Personal Blog"
+
+    subscribers = SubscriberForm()
+    if subscribers.validate_on_submit():
+        subscriber = Subscribers(email = subscribers.email.data)
+        db.session.add(subscriber)
+        db.session.commit()
+        flash('You are now subscribed')
+        mail_message("Welcome to my blog","email/welcome",subscriber.email,subscriber=subscriber)
+        return redirect(url_for('main.index'))
+
+    return render_template('index.html', title=title, subscribers=subscribers)
