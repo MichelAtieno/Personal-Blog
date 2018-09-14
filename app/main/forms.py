5579e4ab73e18ea88f9d27b3a52204b6b78d8fa7
Michel Atieno <mishqamish@gmail.com>
2018-09-14 17:59:50 +0300

    Added BlogPostForm and method for creating blog post

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,7 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import Required
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import Required, DataRequired, Length
+from app.models import BlogPost, Subscribers
 
 class SubscriberForm(FlaskForm):
     email = StringField('Email', validators=[Required()])
     subscribe = SubmitField('Subscribe')
+
+class BlogPostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired(),Length(min=1, max=1000)])
+    blog_content= TextAreaField('Post your blog', validators=[DataRequired(), Length(min=1)])
+    submit = SubmitField('Submit')
--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,10 @@
 from flask import render_template, redirect, url_for,flash
 from . import main
-from .forms import SubscriberForm
-from ..models import User, Subscribers
+from .forms import SubscriberForm, BlogPostForm
+from ..models import User, Subscribers, BlogPost
 from ..email import mail_message
 from .. import db
+from flask_login import login_required, current_user
 
 @main.route('/',methods=['GET', 'POST'])
 def index():
@@ -23,3 +24,19 @@ def index():
         return redirect(url_for('main.index'))
 
     return render_template('index.html', title=title, subscribers=subscribers)
+
+@main.route('/create_blogpost', methods = ['GET', 'POST'])
+@login_required
+def create_blogpost():
+    blog_form = BlogPostForm()
+
+    if blog_form.validate_on_submit():
+        title= blog_form.title.data
+        blog = blog_form.blog_content.data
+
+        new_blog = BlogPost(title=title, blog_post=blog)
+        new_blog.save_blog()
+
+        return redirect(url_for('main.blog',id=new_blog.id))
+    
+    return render_template('create_blog.html', blog_form=blog_form)
